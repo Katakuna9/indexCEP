74280313626081004ac217b2e292920c3f902d20
Katakuna9 <91599882+Katakuna9@users.noreply.github.com>
2021-11-08 13:35:27 +0100

    Add files via upload

---
+++ projecteCEP/ficherosJsfiddle.js
@@ -0,0 +1,15 @@
+function contador() {
+    let resul = "  //jsfiddle.net/katakuna/0tha792x/99/embed/";
+    let doc = document.getElementById("result");
+    doc.setAttribute("src",resul);
+}
+function formulario() {
+    let resul = "//jsfiddle.net/katakuna/xa39rmh0/252/embed/";
+    let doc = document.getElementById("result");
+    doc.setAttribute("src",resul);
+}
+function inputs() {
+    let resul = "  //jsfiddle.net/katakuna/bpcwnx7h/92/embed/";
+    let doc = document.getElementById("result");
+    doc.setAttribute("src",resul);
+}
